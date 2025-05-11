233068f0324ce585498316c28b2ca85444384b82
NensiiP <nensipatoliya99@gmail.com>
2025-05-11 16:09:05 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";  // Change if using a different host
-$username = "root";         // Your database username
-$password = "";             // Your database password
+$servername = "db4free.net";  // Change if using a different host
+$username = "novena_hospital";         // Your database username
+$password = "novena_hospital";             // Your database password
 $dbname = "novena_hospital";    // Your database name
 
 // Create connection
